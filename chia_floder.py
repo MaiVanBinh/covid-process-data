fc8d9f8da8ac1e373fc81ac74dc4b16b723f9c65
binh mai <maivanbinh1321999@gmail.com>
2021-09-28 09:55:02 +0700

    1st

--- chia_floder.py
+++ chia_floder.py
@@ -8,7 +8,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 import tensorflow
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
