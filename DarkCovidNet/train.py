4d00daf7de5b4da25f9c6daf2ae4910d4142bc03
binh mai <maivanbinh1321999@gmail.com>
2021-10-17 08:10:41 +0700

    add DarkCovidNet

--- DarkCovidNet/train.py
+++ DarkCovidNet/train.py
@@ -5,10 +5,10 @@ import fastai
 path = Path('/home/binh/covid-chestxray-dataset/base_dir/')
 
 np.random.seed(41)
-data = ImageDataBunch.from_folder(path, train="train", valid ="valid",
-        ds_tfms=get_transforms(), size=(256,256), bs=32, num_workers=4).normalize()
+# data = ImageDataBunch.from_folder(path, train="train", valid ="valid",
+#         ds_tfms=get_transforms(), size=(256,256), bs=32, num_workers=4).normalize()
 
-print(data.classes)
-print(data.c)
-print(data.train_ds)
-print(data.valid_ds)
+# print(data.classes)
+# print(data.c)
+# print(data.train_ds)
+# print(data.valid_ds)
