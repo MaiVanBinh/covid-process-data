b378969a4bbd9608aed96c0c89b7598aecbf491d
binh mai <maivanbinh1321999@gmail.com>
2021-10-17 08:12:43 +0700

    add DarkCovidNet

--- DarkCovidNet/train.py
+++ DarkCovidNet/train.py
@@ -8,7 +8,7 @@ np.random.seed(41)
 data = ImageDataBunch.from_folder(path, train="train_dir", valid ="val_dir",
         ds_tfms=get_transforms(), size=(256,256), bs=32, num_workers=4).normalize()
 
-# print(data.classes)
-# print(data.c)
-# print(data.train_ds)
-# print(data.valid_ds)
+print(data.classes)
+print(data.c)
+print(data.train_ds)
+print(data.valid_ds)
