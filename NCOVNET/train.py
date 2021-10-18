b720abee03fec713c50d164354979b614e560bbb
binh mai <maivanbinh1321999@gmail.com>
2021-10-18 09:08:51 +0700

    add ncovnet

---
+++ NCOVNET/train.py
@@ -0,0 +1,99 @@
+import tensorflow as tf
+import keras
+
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import VGG16,InceptionV3,ResNet50, MobileNetV2
+from keras.applications.vgg16 import preprocess_input
+#from keras.applications.mobilenetv2 import preprocess_input
+#from keras.applications.resnet50 import preprocess_input
+from keras.preprocessing import image
+from tensorflow.keras.layers import AveragePooling2D, Dropout,Flatten, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import cv2
+import os
+import pandas as pd
+from PIL import Image
+import time
+
+train_path = "./gdrive/MyDrive/covid/v2/code/nCOVnet-COVID-19-detector/dataset/train"
+test_path = "./gdrive/MyDrive/covid/v2/code/nCOVnet-COVID-19-detector/dataset/test"
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+ rotation_range=20,
+ featurewise_center = True,
+ featurewise_std_normalization = True,
+ width_shift_range=0.2,
+ height_shift_range=0.2,
+ shear_range=0.25,
+ zoom_range=0.1,
+ zca_whitening = True,
+ channel_shift_range = 20,
+ horizontal_flip = True ,
+ vertical_flip = True ,
+ validation_split = 0.2)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+train_generator = train_datagen.flow_from_directory(train_path,
+    target_size = (224, 224),
+    shuffle=True,seed=42,class_mode="categorical",
+    color_mode = 'rgb',
+    batch_size = 16)
+test_generator = test_datagen.flow_from_directory(test_path,
+    target_size = (224, 224),
+    color_mode = 'rgb',
+    batch_size = 1,seed=42,class_mode="categorical",
+    shuffle = False)
+
+all_labels = ['COVID-19','other']
+t_x, t_y = next(train_generator)
+fig, m_axs = plt.subplots(4, 4, figsize = (16, 16))
+for (c_x, c_y, c_ax) in zip(t_x, t_y, m_axs.flatten()):
+    c_ax.imshow(c_x[:,:,0], cmap = 'bone', vmin = -1.5, vmax = 1.5)
+    c_ax.set_title(', '.join([n_class for n_class, n_score in zip(all_labels, c_y) 
+                             if n_score>0.5]))
+    c_ax.axis('off')
+
+basemodel = VGG16(weights="imagenet", include_top=False,input_tensor=Input(shape=(224, 224, 3)))
+
+headModel = basemodel.output
+headModel = AveragePooling2D(pool_size=(4, 4))(headModel)#pool_size=(4, 4)
+headModel = Flatten(name="flatten")(headModel)
+#headModel = Dropout(0.5)(headModel)
+headModel = Dense(256, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+#headModel = Dense(256, activation="relu")(headModel)
+#headModel = Dropout(0.3)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+model = Model(inputs=basemodel.input, outputs=headModel)
+
+for layer in basemodel.layers:
+    layer.trainable = False
+
+epochs= 80
+lr = 1e-4
+BS = 16
+
+model.compile(loss="categorical_crossentropy", optimizer=Adam(lr=lr,decay=lr/epochs),metrics=["accuracy"])
+
+start_time = time.time()
+
+
+H = model.fit_generator(train_generator,
+                    steps_per_epoch = 200//BS,
+                    epochs = epochs,
+                    validation_data = test_generator,
+                    validation_steps = 84//BS)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+print("[INFO] saving COVID-19 detector model...")
+model.save('nCOVnet.h5', save_format="h5")
